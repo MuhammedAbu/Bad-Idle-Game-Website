5d08027d51363d038ecc182600e056e2dc9934d6
Muhammed Abushamma <112053604+MuhammedAbu@users.noreply.github.com>
2024-12-29 17:34:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for handling icon click
+function iconClicked() {
+    // Open the GitHub page in a new tab
+    window.open("https://github.com/BAD-IDLE-Game/REALLY-kinda-BAD-IDLE-GAME-Just-Kidding-The-Prequel-II", "_black");
+  }
+  
